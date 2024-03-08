fddd8794ce2c547a44a236245f718091fe69bc21
ordinov <giovanni@cellie.it>
2024-03-08 22:08:49 +0100

    custom footer with widgets

--- footer.php
+++ footer.php
@@ -30,184 +30,40 @@
                 </div>
             </div>
 
-            <?php if (is_active_sidebar('limbiate-footer-area')) : ?>
-                <div class="row">
-                    <div class="col-12 footer-items-wrapper logo-wrapper">
-                        <div id="footer-widget-area" class="footer-widget-area widget-area limbiate-widget-area-container" role="complementary">
-                            <?php dynamic_sidebar('limbiate-footer-area'); ?>
-                        </div>
+            <div id="footer-widget-area" class="row">
+                <div class="hr"><hr></div>
+                <?php if (is_active_sidebar('limbiate-footer-area-1')) { ?>
+                    <div class="col-md-4 footer-items-wrapper">
+                        <?php dynamic_sidebar('limbiate-footer-area-1'); ?>
                     </div>
-                </div>
-                <hr>
-            <?php endif; ?>
+                <?php } ?>
+                <?php if (is_active_sidebar('limbiate-footer-area-2')) { ?>
+                    <div class="col-md-4 footer-items-wrapper">
+                        <?php dynamic_sidebar('limbiate-footer-area-2'); ?>
+                    </div>
+                <?php } ?>
+                <?php if (is_active_sidebar('limbiate-footer-area-3')) { ?>
+                    <div class="col-md-4 footer-items-wrapper">
+                        <?php dynamic_sidebar('limbiate-footer-area-3'); ?>
+                    </div>
+                <?php } ?>
+            </div>
+            <hr>
 
-            <div class="row">
-                <div class="col-md-3 footer-items-wrapper">
-                    <?php
-                    $location = "menu-footer-col-1";
-                    if (has_nav_menu($location)) { ?>
-                        <h3 class="footer-heading-title">
-                            <?php echo wp_get_nav_menu_name($location); ?>
-                        </h3>
-                    <?php wp_nav_menu(array(
-                            "theme_location" => $location,
-                            "depth" => 0,
-                            "menu_class" => "footer-list",
-                            'walker' => new Footer_Menu_Walker()
-                        ));
-                    }
-                    ?>
-                </div>
-                <div class="col-md-6 footer-items-wrapper">
-                    <?php
-                    $location = "menu-footer-col-2";
-                    if (has_nav_menu($location)) {
-                        $theme_locations = get_nav_menu_locations();
-                        $menu = get_term($theme_locations[$location], 'nav_menu');
-                        $menu_count = $menu->count;
-                    ?>
-                        <h3 class="footer-heading-title">
-                            <?php echo wp_get_nav_menu_name($location); ?>
-                        </h3>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <?php wp_nav_menu(array(
-                                    "theme_location" => $location,
-                                    "depth" => 0,
-                                    "menu_class" => "footer-list",
-                                    "li_slice" => array(0, ceil($menu_count / 2)),
-                                    'walker' => new Footer_Menu_Walker()
-                                )); ?>
-                            </div>
-                            <div class="col-md-6">
-                                <?php wp_nav_menu(array(
-                                    "theme_location" => $location,
-                                    "depth" => 0,
-                                    "menu_class" => "footer-list",
-                                    "li_slice" => array(ceil($menu_count / 2), $menu_count),
-                                    'walker' => new Footer_Menu_Walker()
-                                )); ?>
-                            </div>
-                        </div>
-                    <?php } ?>
-                </div>
-                <div class="col-md-3 footer-items-wrapper">
-                    <?php
-                    $location = "menu-footer-col-3-1";
-                    if (has_nav_menu($location)) { ?>
-                        <h3 class="footer-heading-title">
-                            <?php echo wp_get_nav_menu_name($location); ?>
-                        </h3>
-                    <?php wp_nav_menu(array(
-                            "theme_location" => $location,
-                            "depth" => 0,
-                            "menu_class" => "footer-list",
-                            "container_class" => "footer-list",
-                            'walker' => new Footer_Menu_Walker()
-                        ));
-                    }
-                    ?>
-                    <?php
-                    $location = "menu-footer-col-3-2";
-                    if (has_nav_menu($location)) { ?>
-                        <h3 class="footer-heading-title">
-                            <?php echo wp_get_nav_menu_name($location); ?>
-                        </h3>
-                    <?php wp_nav_menu(array(
-                            "theme_location" => $location,
-                            "depth" => 0,
-                            "menu_class" => "footer-list",
-                            'walker' => new Footer_Menu_Walker()
-                        ));
-                    }
-                    ?>
-                </div>
-                <div class="col-md-9 mt-md-4 footer-items-wrapper">
-                    <h3 class="footer-heading-title">Contatti</h3>
-                    <div class="row">
-                        <div class="col-md-4">
-                            <p class="footer-info">
-                                <?php echo dci_get_option("nome_comune"); ?>
-                                <br /><?php echo dci_get_option("contatti_indirizzo", 'footer'); ?>
-                                <br /><?php if (dci_get_option("contatti_CF_PIVA", 'footer')) echo 'Codice fiscale / P. IVA:' . dci_get_option("contatti_CF_PIVA", 'footer'); ?>
-                                <br /><br />
-                                <?php
-                                $ufficio_id = dci_get_option("contatti_URP", 'footer');
-                                $ufficio = get_post($ufficio_id);
-                                if ($ufficio_id) { ?>
-                                    <a href="<?php echo get_post_permalink($ufficio_id); ?>" class="list-item" title="Vai alla pagina: URP">
-                                        <?php echo $ufficio->post_title ?>
-                                    </a>
-                                <?php } ?>
-                                <?php if (dci_get_option("numero_verde", 'footer')) echo '<br />Numero verde: ' . dci_get_option("numero_verde", 'footer'); ?>
-                                <?php if (dci_get_option("SMS_Whatsapp", 'footer')) echo '<br />SMS e WhatsApp: ' . dci_get_option("SMS_Whatsapp", 'footer'); ?>
-                                <?php
-                                if (dci_get_option("contatti_PEC", 'footer')) echo '<br />PEC: '; ?>
-                                <a href="mailto:<?php echo dci_get_option("contatti_PEC", 'footer'); ?>" class="list-item" title="PEC <?php echo dci_get_option("nome_comune"); ?>"><?php echo dci_get_option("contatti_PEC", 'footer'); ?></a>
-                                <?php if (dci_get_option("centralino_unico", 'footer')) echo '<br />Centralino unico: ' . dci_get_option("centralino_unico", 'footer'); ?>
-                            </p>
-                        </div>
-                        <div class="col-md-4">
-                            <?php
-                            $location = "menu-footer-info-1";
-                            if (has_nav_menu($location)) {
-                                wp_nav_menu(array(
-                                    "theme_location" => $location,
-                                    "depth" => 0,
-                                    "menu_class" => "footer-list",
-                                    'walker' => new Footer_Menu_Walker()
-                                ));
-                            }
-                            ?>
-                        </div>
-                        <div class="col-md-4">
-                            <?php
-                            $location = "menu-footer-info-2";
-                            if (has_nav_menu($location)) {
-                                wp_nav_menu(array(
-                                    "theme_location" => $location,
-                                    "depth" => 0,
-                                    "menu_class" => "footer-list",
-                                    'walker' => new Footer_Menu_Walker()
-                                ));
-                            }
-                            ?>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 mt-md-4 footer-items-wrapper">
-                    <?php
-                    $socials = dci_get_option('link_social', 'socials');
-                    if (is_array($socials) && count($socials)) {
-                    ?>
-                        <h3 class="footer-heading-title">Seguici su</h3>
-                        <ul class="list-inline text-start social">
-                            <?php foreach ($socials as $social) { ?>
-                                <li class="list-inline-item">
-                                    <a href="<?php echo $social['url_social'] ?>" target="_blank" class="p-2 text-white">
-                                        <svg class="icon icon-sm icon-white align-top">
-                                            <use xmlns:xlink="http://www.w3.org/1999/xlink" href="#<?php echo $social['icona_social'] ?>"></use>
-                                        </svg>
-                                        <span class="visually-hidden"><?php echo $social['nome_social']; ?></span>
-                                    </a>
-                                </li>
+            <?php if (dci_get_option("media_policy", 'footer') || dci_get_option("sitemap", 'footer')) { ?>
+                <div class="row">
+                    <div class="col-12 footer-items-wrapper">
+                        <div class="footer-bottom">
+                            <?php if (dci_get_option("media_policy", 'footer')) { ?>
+                                <a href="<?php echo dci_get_option("media_policy", 'footer'); ?>">Media policy</a>
                             <?php } ?>
-                        </ul><!-- /header-social-wrapper -->
-                    <?php } ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 footer-items-wrapper">
-                    <div class="footer-bottom">
-                        <?php if (dci_get_option("media_policy", 'footer')) { ?>
-                            <a href="<?php echo dci_get_option("media_policy", 'footer'); ?>">Media policy</a>
-                        <?php } ?>
-                        <?php if (dci_get_option("sitemap", 'footer')) { ?>
-                            <a href="<?php echo dci_get_option("sitemap", 'footer'); ?>">Mappa del sito</a>
-                        <?php } ?>
+                            <?php if (dci_get_option("sitemap", 'footer')) { ?>
+                                <a href="<?php echo dci_get_option("sitemap", 'footer'); ?>">Mappa del sito</a>
+                            <?php } ?>
+                        </div>
                     </div>
                 </div>
-            </div>
+            <?php } ?>
         </div>
     </div>
 </footer>
--- widgets/add-footer-area.php
+++ widgets/add-footer-area.php
@@ -1,14 +1,33 @@
 <?php
 
-function add_limbiate_footer_widget_area() {
-    register_sidebar( array(
-        'name'          => esc_html__( 'Limbiate Footer', 'text_domain' ),
-        'id'            => 'limbiate-footer-area',
-        'description'   => 'Aggiungi widgets che appariranno nel footer',
-        'before_widget' => '<section id="%1$s" class="widget %2$s limbiate-footer-widget">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h2 class="widget-title limbiate-footer-title">',
-        'after_title'   => '</h2>',
-    ) );
+function add_limbiate_footer_widgets()
+{
+    register_sidebar([
+        'name'          => esc_html__('Limbiate Footer COL 1', 'text_domain'),
+        'id'            => 'limbiate-footer-area-1',
+        'description'   => 'Aggiungi widgets che appariranno nella colonna 1 del footer',
+        'before_widget' => '<div id="%1$s" class="widget %2$s limbiate-footer-widget limbiate-footer-col1">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="footer-heading-title">',
+        'after_title'   => '</h3>',
+    ]);
+    register_sidebar([
+        'name'          => esc_html__('Limbiate Footer COL 2', 'text_domain'),
+        'id'            => 'limbiate-footer-area-2',
+        'description'   => 'Aggiungi widgets che appariranno nella colonna 2 del footer',
+        'before_widget' => '<div id="%1$s" class="widget %2$s limbiate-footer-widget limbiate-footer-col2">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="footer-heading-title">',
+        'after_title'   => '</h3>',
+    ]);
+    register_sidebar([
+        'name'          => esc_html__('Limbiate Footer COL 3', 'text_domain'),
+        'id'            => 'limbiate-footer-area-3',
+        'description'   => 'Aggiungi widgets che appariranno nella colonna 3 del footer',
+        'before_widget' => '<div id="%1$s" class="widget %2$s limbiate-footer-widget limbiate-footer-col3">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="footer-heading-title">',
+        'after_title'   => '</h3>',
+    ]);
 }
-add_action( 'widgets_init', 'add_limbiate_footer_widget_area' );
+add_action('widgets_init', 'add_limbiate_footer_widgets');
