e402889ebf9e7c5a8fd8f69bc85e8a3f02e68e03
ordinov <giovanni@cellie.it>
2024-07-04 19:22:10 +0200

    update

--- limbiate.js
+++ limbiate.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const stickyHeader = document.getElementById('sticky-header');
 
     function toggleStickyHeader() {
-        if (window.scrollY > 0) {
-            console.log('oooo')
+        if (window.scrollY > 200) {
             stickyHeader.classList.remove('sticky-hidden');
         } else {
             stickyHeader.classList.add('sticky-hidden');
