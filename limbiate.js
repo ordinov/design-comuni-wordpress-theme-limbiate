ed7e7ddd816abfeeffa0b147c4173a0c2d1a4d15
ordinov <giovanni@cellie.it>
2024-07-16 16:47:44 +0200

    update

--- limbiate.js
+++ limbiate.js
@@ -2,13 +2,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const stickyHeader = document.getElementById('sticky-header');
     function toggleStickyHeader() {
-        if (window.scrollY > 200) {
+        const viewportWidth = window.innerWidth;
+        if (viewportWidth >= 995 && window.scrollY > 200) {
             stickyHeader.classList.remove('sticky-hidden');
         } else {
             stickyHeader.classList.add('sticky-hidden');
         }
     }
     window.addEventListener('scroll', toggleStickyHeader);
+    window.addEventListener('resize', toggleStickyHeader);
     toggleStickyHeader();
 });
 
